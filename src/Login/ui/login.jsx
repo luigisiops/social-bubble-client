598106312635ef4df96decff25b1fc1c27be6830
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2020-12-02 03:33:19 -0500

    working login and register UI

--- src/Login/ui/login.jsx
+++ src/Login/ui/login.jsx
@@ -1,30 +1,51 @@
 import React, {useState} from "react"
+import { NavLink, Redirect } from "react-router-dom"
+import { connect } from "react-redux"
 import "./login.css"
 
 export const Login = () => {
+    const [fields, setFields] = useState({})
+
+    const [loginStatus, setloginStatus] = useState({})
+    const setField = (evt) => 
+        setFields({
+            ...fields,
+            [evt.target.name]: evt.target.value
+        })
+    console.log(fields)
+    
     return(
         <div className = "login-container">
             <div className = "logo">Social Bubble</div>
             <div className = "login-form">
                 <div className = "username-container">
                     <label>Username</label>
-                    <input className = "username-input" type = "text"></input>
+                    <input className = "username-input" 
+                    name = "email" 
+                    type = "email"
+                    value = {fields.email}
+                    onChange = {setField}>
+                    </input>
                 </div>
                 <div className = "password-container">
                     <label>Password</label>
-                    <input className = "password-input" type = "text"></input>
+                    <input className = "password-input" 
+                    name = "password"
+                    type = "password"
+                    value = {fields.password}
+                    onChange = {setField}>
+                    </input>
                 </div>
                
                 <button className = "signIn-button">Sign In</button>
             </div>
             <div className = "signup-link">
-                <p>Not a user? Sign up here!</p>
+            <p>Not a user? <NavLink to="/register/">Register</NavLink></p>
             </div>
-            
-            
-            
         </div>
     )
 }
+
+
 
 export default Login
