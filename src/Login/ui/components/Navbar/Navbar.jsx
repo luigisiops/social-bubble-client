09ac3164685f6c77c4621d93b8f0c4e8d06603ad
Matthew Quint <mattquint111@gmail.com>
2020-12-03 11:28:57 -0600

    Navbar

--- src/Login/ui/components/Navbar/Navbar.jsx
+++ src/Login/ui/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router'
+
 
 function Navbar() {
 
     return (
         <div className="navbar-container">
             <h1>NAVBAR</h1>
+            <h2><Link to='/bubbles'>Bubbles</Link></h2>
+            <h2><Link to='/bubble-builder'>Bubble Builder</Link></h2>
         </div>
     )
 }
