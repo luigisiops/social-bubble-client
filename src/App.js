56efa6b6830a59720a39069f9378ebed649a574e
NathanNOSudo <nathandavid719@gmail.com>
2020-12-06 15:35:26 -0600

    added redux middleware, thunk, logger. restructured *

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch, Provider } from 'react-redux';
+import { verifyToken } from './actions/userActions.js'
+
 import './App.css';
 
-import React from "react"
-import { Provider } from "react-redux"
-import store from "./common/redux/store"
-import {
-  Redirect,
-  Route,
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom"
+// import store from "./common/redux/store"
+// import {
+//   Redirect,
+//   Route,
+//   BrowserRouter as Router,
+//   Switch,
+// } from "react-router-dom"
 
+/* COMPONENTS  */
+import {PrivateRoute} from './components/ProtectedRoute.js';
+import Home from './components/Home.js';
+import Entry from './components/Entry.js';
+import Nav from './components/Nav.js';
+import Footer from './components/Footer.js';
+import SignIn from './components/forms/SignIn.js';
+import SignUp from './components/forms/SignUp.js';
+import NewEntry from './components/forms/NewEntry.js';
 import Login from './login/ui/login'
 import Register from './login/ui/register'
-
 import Bubbles from './Bubbles/ui/Bubbles'
 import BubbleBuilder from './BubbleBuilder/ui/BubbleBuilder';
 import Dashboard from './Bubbles/ui/Dashboard';
 import Members from './Bubbles/ui/Members'
 
+export default function App() {
+  const dispatch = useDispatch();
 
-const App = () => {
+  useEffect(() => {
+    dispatch(verifyToken());
+  }, [dispatch])
+  
   return (
+    <div>
     <Provider store = {store}>
       <Router>
         <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/entry/:id" component={Entry} />
+          <Route exact path="/page/:page" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
           <Route exact path = "/login" component={Login} />
           <Route exact path = "/register" component={Register} />
           <Route exact path = '/bubbles' component={Bubbles} />
           <Route exact path = '/bubble-builder' component={BubbleBuilder} />
           <Route exact path = '/dashboard' component = {Dashboard} />
           <Route exact path = '/members' component = {Members} />
+          <PrivateRoute exact path="/new" component={NewEntry} />
         </Switch>
       </Router>
     </Provider>
+    </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from './reducers';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister(); 
